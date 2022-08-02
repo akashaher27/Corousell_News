deef9be4e877bf2b8ec59229062e34046fa5eded
akash <akash.aher@mediassist.in>
2022-08-02 11:53:29 +0530

    added common module

---
+++ common/src/main/java/com/example/common/util/SystemUtils.kt
@@ -0,0 +1,52 @@
+package com.example.common.util
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.Uri
+import android.provider.OpenableColumns
+import androidx.core.content.FileProvider
+import com.example.common.model.FileDetail
+import java.io.File
+
+/**
+ * Created by akash on 22,11,2021
+ */
+
+
+fun getTempFileUri(context: Context): Uri? {
+    val tempFile = File.createTempFile("temp", null, context.cacheDir)
+    return try {
+        FileProvider.getUriForFile(
+            context.applicationContext,
+            "com.example.testproject.provider",
+            tempFile
+        )
+    } catch (e: Exception) {
+        null
+    }
+}
+
+fun getFileDetail(context: Context, uri: Uri): FileDetail {
+    var name: String? = null
+    var size: Long? = null
+    context.contentResolver.query(uri, null, null, null, null)?.use { cursor ->
+        val nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+        val sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE)
+        cursor.moveToFirst()
+        name = cursor.getString(nameIndex).toString()
+        size = cursor.getLong(sizeIndex)
+    }
+    return FileDetail(name, size, uri)
+}
+
+fun toMegaByte(byte: Long): Float {
+    return (byte / (1024 * 1024)).toFloat()
+}
+
+fun Context.isInternetConnected(): Boolean {
+    val manager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val networkCapabilities = manager.getNetworkCapabilities(manager.activeNetwork)
+    return networkCapabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) ?: false
+}
+
